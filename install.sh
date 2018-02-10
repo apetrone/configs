12b59c3dafcf5308d73c008e41bce989ceae2c54
Adam Petrone <adampetrone83@gmail.com>
2018-02-10 11:50:32 -0500

    Add install for gitconfig

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cp sublimetext2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
+
+# Replace user's gitconfig.
+cp git/git_config ~/.gitconfig
+
