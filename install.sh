b096a4260695a3944b197dcedbe48586100069c5
Adam Petrone <adampetrone83@gmail.com>
2018-02-26 23:50:03 -0500

    update for sublimetext3

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-cp sublimetext2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/Preferences.sublime-settings
+if [ -d "${HOME}/.config/sublime-text-2" ]; then
+    echo "Installing sublime-text-2 preferences..."
+    cp sublimetext2/Preferences.sublime-settings ~/.config/sublime-text-2/Packages/User/
+fi
+
+if [ -d "${HOME}/.config/sublime-text-3" ]; then
+    echo "Installing sublime-text-3 preferences..."
+    cp sublimetext3/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
+fi
+
 
 # Replace user's gitconfig.
+echo "Installing git preferences..."
 cp git/git_config ~/.gitconfig
 
